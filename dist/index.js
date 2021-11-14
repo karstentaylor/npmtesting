1bc8b9a3b2dd744c9cbace2548c32e55389cf5b7
Karsten Taylor <karstentaylor0@gmail.com>
2021-11-14 13:52:08 -0800

    adding to package.json

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var _NftCard = _interopRequireDefault(require("./components/NftCard"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
